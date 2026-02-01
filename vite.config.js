3e514112b4e78e8ec6d8fed6f9229dd76b202e80
iamsaba786 <snaz7571@gmail.com>
2026-02-01 11:25:52 +0530

    fix base path for vercel

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/",
+  base: "/Amul-redesign/",
   plugins: [react(), tailwindcss()],
 });
