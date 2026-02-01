010da824d21eb50927d7ad6bb71ba32f3064b043
iamsaba786 <snaz7571@gmail.com>
2026-02-01 11:31:37 +0530

    fix kar dia vercel

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/Amul-redesign/",
+  base: "/",
   plugins: [react(), tailwindcss()],
 });
